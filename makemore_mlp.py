0320713e41406062f820b528657db73f47a67831
Manoj Ravindran <manojravindran@Manojs-MacBook-Pro.local>
2024-01-11 18:13:16 -0800

    making code more pytorch like

--- makemore_mlp.py
+++ makemore_mlp.py
@@ -48,53 +48,118 @@ print('X_dev.shape -->', X_dev.shape, 'Y_dev.shape -->', Y_dev.shape)
 
 
 # %%
+# Make the code more pytorch like
+
+g = torch.Generator().manual_seed(2147483647)
+
+class Linear:
+    def __init__(self, n_in, n_out, bias=True):
+        self.weights = torch.randn(n_in, n_out, generator=g) / (n_in ** 0.5)
+        self.bias = torch.randn(n_out) if bias else None
+
+    def __call__(self, x):
+        self.out = x @ self.weights
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+    
+    def parameters(self):
+        if self.bias is not None:
+            return [self.weights, self.bias]
+        else:
+            return [self.weights]
+
+
+class BatchNorm1d:
+    def __init__(self, dim, eps=1e-5, moementum=0.1):
+        self.eps = eps
+        self.moementum = moementum
+        self.training = True
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+        return None
+    
+    def __call__(self, x):
+        if self.training:
+            xmean = x.mean(dim=0, keepdim=True)
+            xvar = x.var(dim=0, keepdim=0)
+            self.running_var = (1-self.moementum) * self.running_var + self.moementum * xvar
+            self.running_mean = (1-self.moementum) * self.running_mean + self.moementum * xvar
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+        xhat = (x-xmean) / ((xvar + self.eps) ** 0.5)
+        self.out = (self.gamma * xhat) + self.beta
+        return self.out
+    
+    def parameters(self):
+        return [self.gamma, self.beta]
+    
+class Tanh:
+
+    def __call__(self, x):
+        self.out = torch.tanh(x)
+        return self.out
+    
+    def parameters(self):
+        return []
+
+# %%
 # network parameters
 n_embd = 10
-n_hidden = 200
+n_hidden = 100
 batch_size = 32
-g = torch.Generator().manual_seed(2147483647)
 C = torch.randn(vocab_size, n_embd, generator=g) 
-W1 = torch.randn(n_embd * context_len, n_hidden, generator=g) * (5/3) / ((n_embd * context_len) ** 0.5) # 5/3 since the tanh saturates at 5/3
-# b1 = torch.randn(n_hidden, generator=g)# * 0.01
-W2 = torch.randn(n_hidden, vocab_size, generator=g) * 0.01
-b2 = torch.randn(vocab_size, generator=g) * 0
-bngain = torch.ones(n_hidden)
-bnbias = torch.zeros(n_hidden)
-bnrunning_mean = torch.zeros(n_hidden)
-bnrunning_std = torch.ones(n_hidden)
-parameters = [C, W1, W2, b2, bngain, bnbias]
+
+layers = [
+    Linear(n_embd * context_len, n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), Tanh(),
+    Linear(n_hidden, vocab_size),
+]
+
+# make the softmax layer less confident in its initial predictions
+with torch.no_grad():
+    layers[-1].weights *= 0.1
+# boost the linear layers before the tanh by 5/3 in order to fight the squashing
+for layer in layers[:-1]:
+    if isinstance(layer, Linear):
+        layer.weights *= 5/3
+
+parameters = [p for layer in layers for p in layer.parameters()] + [C]
 print(sum(p.nelement() for p in parameters))
 for p in parameters:
     p.requires_grad = True
 
 
 # %%
-# model
+#model
 max_steps = 200000
 batch_size = 32
 lossi = []
 
 for i in range(max_steps):
+    #sampling the batch
     idx = torch.randint(0 ,X_train.shape[0], (batch_size,), generator=g) # (batch_size,)
     Xb, Yb = X_train[idx], Y_train[idx] # (batch_size, context_len), (batch_size,)
+ 
+    #forward pass
     emb = C[Xb]  # (batch_size, context_len, n_embd)
     embcat = emb.view(emb.shape[0], -1) # (batch_size, n_embd * context_len)
-    hpreact = embcat @ W1# + b1 # (batch_size, n_hidden)
-    # adding batch normalization
-    bnmeani = hpreact.mean(dim=0, keepdim=True)
-    bnstdi = hpreact.std(dim=0, keepdim=True)
-    hpreact = (hpreact - bnmeani) / bnstdi
-    with torch.no_grad():
-        bnrunning_mean = 0.999 * bnrunning_mean + 0.001 * bnmeani
-        bnrunning_std = 0.999 * bnrunning_std + 0.001 * bnstdi
-    hpreact = (bngain  * hpreact) + bnbias
-    h = torch.tanh(hpreact) # (batch_size, n_hidden)
-    logits = h @ W2 + b2 # (batch_size, vocab_size)
+    logits = embcat
+    for layer in layers:
+        logits = layer(logits)
     loss = F.cross_entropy(logits, Yb)
 
+    #backward pass
     for p in parameters:
         p.grad = None
     loss.backward()
+    
     # update parameters
     lr = 0.1 if i < int(max_steps/2) else 0.01
     for p in parameters:
@@ -103,6 +168,7 @@ for i in range(max_steps):
     if i%10000 == 0:
         print(f'{i:7d}/{max_steps:7d}: {loss.item():.4f}')
     lossi.append(loss.log10().item())
+    break
 
 # %%
 # with torch.no_grad():
@@ -123,12 +189,9 @@ def split_loss(split):
     }[split]
     emb = C[x]  # (batch_size, context_len, n_embd)
     embcat = emb.view(emb.shape[0], -1) # (batch_size, n_embd * context_len)
-    hpreact = embcat @ W1 # + b1 # (batch_size, n_hidden)
-    # adding batch normalization
-    hpreact = (hpreact - bnrunning_mean) / bnrunning_std
-    hpreact = (bngain  * hpreact) + bnbias
-    h = torch.tanh(hpreact) # (batch_size, n_hidden)
-    logits = h @ W2 + b2 # (batch_size, vocab_size)
+    logits = embcat
+    for layer in layers:
+        logits = layer(logits)
     loss = F.cross_entropy(logits, y)
     print(f'{split} loss: {loss:.4f}')
 
